8413b5c53dda3864054344a88777453f7335661f
bllew-student <bllewellyn.student@gmail.com>
2026-04-22 05:50:33 +0000

    added implementation notes for Queue2.js

---
+++ src/toolkit/Queue2.js
@@ -0,0 +1,10 @@
+/*
+* Implement a queue using a linked list. 
+* enqueue() should add an item to the back of the queue by creating a 
+*   new node and updating the queue's tail pointer.
+* dequeue() should remove and return the item from the front of the queue by
+*   updating the queue's head pointer to the next node.
+* isEmpty() should return true if the queue has no items, and false otherwise.
+* size() should return the number of items in the queue.
+* The queue should be able to hold any type of value.
+*/
